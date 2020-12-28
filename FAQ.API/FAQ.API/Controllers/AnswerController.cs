626878bf28400694a36e62552aa141a45fd91118
humeau_y <humeau_y@etna-alternance.net>
2020-12-28 22:34:41 +0100

    Clean usings

--- FAQ.API/FAQ.API/Controllers/AnswerController.cs
+++ FAQ.API/FAQ.API/Controllers/AnswerController.cs
@@ -6,10 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Mime;
-using System.Threading.Tasks;
 
 namespace FAQ.API.Controllers
 {
